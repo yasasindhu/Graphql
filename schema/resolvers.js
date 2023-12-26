bacb75c2f309ac4799706e5035991caf0000dee8
yasasindhu <yasasindhu32@gmail.com>
2023-12-26 19:41:15 +0530

    add some complex resolvers

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,9 +1,33 @@
 
-const {UserList}=require("../mockData");
-const resolvers={
-    Query:{
-        users(_,args){
+const { UserList, MovieList } = require("../mockData");
+const _ = require("lodash");
+
+const resolvers = {
+    Query: {
+        users(_, args) {
             return UserList;
-        }
+        },
+        user(parent, args) {
+            const id = args.id;
+            const user = _.find(UserList, { id: Number(id) });
+            return user;
+        },
+        movies: () => {
+            return MoviesList;
+        },
+        movie: (parent, args) => {
+            const name = args.name;
+            const movie = _.find(MovieList, { name });
+            return movie;
+        },
+    },
+    User: {
+        favoriteMovies: () => {
+            return _.filter(
+                MovieList,
+                (movie) =>
+                    movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+            );
+        },
     }
 }
