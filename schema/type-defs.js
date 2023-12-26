0f923d60c65695d1246e7a14b8f2fe837df76e9a
yasasindhu <yasasindhu32@gmail.com>
2023-12-26 22:40:38 +0530

    add mutations

--- schema/type-defs.js
+++ schema/type-defs.js
@@ -24,6 +24,25 @@ const typeDefs = gql`
         movies:[Movie!]!
         movie(name:String!):Movie!
     }
+     
+    input CreateUserInput{
+        name: String!
+        username: String!
+        age:Int!
+        nationality: Nationality = BRAZIL
+    }
+
+    input UpdateUserInput{
+        id: ID!
+        newUsername: String!
+    }
+
+    type Mutations{
+        createUser(input: CreateUserInput!): User!
+        updateUsername(input: UpdateUserInput!): User!
+        deleteUser(id: ID!): User
+    }
+
     enum Nationality{
         CANARA
         BRAZIL
